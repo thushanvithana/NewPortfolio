d6db6dd237308905cf298f7ff6aacd7600c42cf9
thushanvithana <87628351+thushanvithana@users.noreply.github.com>
2023-08-21 09:14:11 +0530

    fgdfgdfg

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,39 +1,3 @@
-// const Footer = () => {
-//   return (
-//     <footer id="footer">
-//       <div className="navigate">
-//         <div className="col col-1">
-//           <div className="footer-banner">
-//             <a href="#">
-//               <img
-//                 src="https://i.postimg.cc/zD0971Dy/Untitled-design.webp"
-//                 alt="logo"
-//                 width="85"
-//                 height="75"
-//               ></img>
-//             </a>
-//           </div>
-//         </div>
-//         <div className="main-bottom-nav">
-//           <div className="bottom-nav">
-//             <ul>
-//               <li>
-//                 <span>
-//                   &copy; Copyright © 2022 Thushan vithana All rights reserved.  
-//                 </span>
-//               </li>
-//               <div className="footer-content"></div>
-//             </ul>
-//           </div>
-//         </div>
-//       </div>
-//     </footer>
-//   );
-// };
-
-// export default Footer;
-
-
 const Footer = () => {
   return (
     <footer id="footer">
@@ -68,4 +32,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
